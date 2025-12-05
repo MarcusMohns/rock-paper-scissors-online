381c097ab4fd25b1d8bc0c1a848f6d6bc9a65b96
Marcus Mohns <marcus@mohns.se>
2025-12-05 16:02:16 +0100

    comment

--- client/src/workers/timerWorker.ts
+++ client/src/workers/timerWorker.ts
@@ -1,5 +1,5 @@
 self.onmessage = function (event) {
-  // Start at 100 let it go to a minus value so we can display 0.
+  // Start at 100 let it go to a minus value so we can display 0 for a sec.
   const { message, delay } = event.data;
   let progress = 100;
   const intervalId = setInterval(() => {
