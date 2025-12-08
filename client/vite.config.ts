965cc575dea2b83cd1eb89873a3e43466aa04f2c
Marcus Mohns <marcus@mohns.se>
2025-12-08 20:19:08 +0100

    remove dev fix for firefox

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ["mui-material-styles"],
-  },
   plugins: [
     react({
       babel: {
